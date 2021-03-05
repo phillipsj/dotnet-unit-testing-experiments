f0a5fe51dc87d568b4120d8ffe403cc3d6410348
Jamie Phillips <cphill11@gmail.com>
2021-03-04 21:30:54 -0500

    Adding side-by-side tests example.

---
+++ src/ExperimentsTestsWithCode/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Shouldly;
+
+namespace ExperimentsTestsWithCode {
+    public class CalculatorTests {
+        public void ShouldAdd() {
+            Calculator.Add(2, 3).ShouldBe(5);
+        }
+
+        public void ShouldSubtract() {
+            Calculator.Subtract(3, 2).ShouldBe(1);
+        }
+
+        public void ShouldMultiply() {
+            Calculator.Multiply(1, 2).ShouldBe(2);
+        }
+
+        public void ShouldDivide() {
+            var data = new[] {
+                new {A = 1, B = 1, Expected = 1, ErrorExpected = false},
+                new {A = 4, B = 2, Expected = 2, ErrorExpected = false},
+                new {A = 4, B = 0, Expected = 0, ErrorExpected = true}
+            };
+
+            foreach (var item in data) {
+                try {
+                    Calculator.Divide(item.A, item.B).ShouldBe(item.Expected);
+                    item.ErrorExpected.ShouldBeFalse();
+                }
+                catch (DivideByZeroException e) {
+                    item.ErrorExpected.ShouldBeTrue();
+                }
+            }
+        }
+    }
+}
